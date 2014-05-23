e5848fbe032f0393231540cb650d4b769a6a40b4
josegargon <josegargon2@gmail.com>
2014-05-23 13:47:12 +0200

    [Sprint 60] translate contact form

--- wp-content/themes/ihorse/includes/shortcodes/aa_contact_form.php
+++ wp-content/themes/ihorse/includes/shortcodes/aa_contact_form.php
@@ -10,12 +10,33 @@ function aa_contact_form($params, $content = null){
 	<div class="form_container">
 		<form id="contactForm" name="contactForm">
 		<ul class="form_fields">
-			<li><input placeholder="Nombre" class="span12" name="contact_name" id="contact_name" type="text" /></li>
-			<li><input placeholder="Email" class="span12" name="contact_email" id="contact_email" type="text" /></li>
-			<li><input placeholder="Resumen" class="span12" name="contact_subject" id="contact_subject" type="text" /></li>
-			<li><textarea placeholder="Mensaje" class="span12" name="contact_message" id="contact_message" cols="30" rows="5"></textarea></li>
+            <?php $var_translate = qtrans_getLanguage();
+            $btnText = 'Senden';
+            $placeholderName = 'Name';
+            $placeholderEmail = 'Email';
+            $placeholderSumary = 'Betreff';
+            $placeholderMessage = 'Nachricht';
+            if($var_translate == 'es') {
+                $btnText = 'Enviar';
+                $placeholderName = 'Nombre';
+                $placeholderEmail = 'Email';
+                $placeholderSumary = 'Resumen';
+                $placeholderMessage = 'Mensaje';
+            } elseif ($var_translate == 'en'){
+                $btnText = 'Send';
+                $placeholderName = 'Name';
+                $placeholderEmail = 'Email';
+                $placeholderSumary = 'Sumary';
+                $placeholderMessage = 'Message';
+            }
+            ?>
+			<li><input placeholder="<?php echo $placeholderName; ?>" class="span12" name="contact_name" id="contact_name" type="text" /></li>
+			<li><input placeholder="<?php echo $placeholderEmail; ?>" class="span12" name="contact_email" id="contact_email" type="text" /></li>
+			<li><input placeholder="<?php echo $placeholderSumary; ?>" class="span12" name="contact_subject" id="contact_subject" type="text" /></li>
+			<li><textarea placeholder="<?php echo $placeholderMessage; ?>" class="span12" name="contact_message" id="contact_message" cols="30" rows="5"></textarea></li>
 		</ul>
-		<button id="btnSendRequest" class="btn btn-inverse btn-large">Enviar</button><span id="request_results"></span>
+
+		<button id="btnSendRequest" class="btn btn-inverse btn-large"><?php echo $btnText ?></button><span id="request_results"></span>
 		</form>
 	</div>
 	<?php 
